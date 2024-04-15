0a5726b8ef3bd0b8b34418ec32ccc0edcec8cf34
Anuj Patil <138184585+AnujPa-teal@users.noreply.github.com>
2024-04-15 16:52:46 +0530

    Update Source_Code.cpp

--- Source_Code.cpp
+++ Source_Code.cpp
@@ -3,14 +3,15 @@
 #include <WiFiClient.h>
 #include <ThingSpeak.h>
 
-#define DHTPIN D2     // connect the output pin of dht11 to D2 pin of NodeMCU
+#define DHTPIN D2     // connect the output pin of dht11 to D2 pin of NodeMCU ESP8266
 
-#define DHTTYPE DHT11
+#define DHTTYPE DHT11    // write here DHT22 in case if you have DHT22 the white coloured sensor
 
-const char *ssid =  "Hotspot/WiFi_SSID";     
-const char *pass = "Hotspot/WiFi_PASSWORD";
-const char* apiKey = "Channel_apikey";
-const unsigned long THINGSPEAK_CHANNEL_ID = channel_id ;
+
+const char *ssid =  "Hotspot/WiFi_SSID";     // enter your wifi/hotspot ssid name
+const char *pass = "Hotspot/WiFi_PASSWORD";   // enter the password of your wifi
+const char* apiKey = "Channel_apikey";        // enter thingspeak channel apiKey
+const unsigned long THINGSPEAK_CHANNEL_ID = channel_id ;    // enter the thingspeak channel id
 const char* server = "api.thingspeak.com";
 
 DHT dht(DHTPIN, DHTTYPE);
@@ -39,14 +40,15 @@ void loop() {
     return;
   }
 
-  ThingSpeak.setField(1, temperature);
-  ThingSpeak.setField(2, humidity);
+  ThingSpeak.setField(1, temperature);    // choose the field 1 in thingspeak channel as temperature
+  ThingSpeak.setField(2, humidity);       // choose the field 2 in thingspeak channel as humidity
 
   int statusCode = ThingSpeak.writeFields(THINGSPEAK_CHANNEL_ID, apiKey);
 
   if (statusCode == 200) {
     Serial.println("Data has been sent to ThingSpeak server successfully!");
-  } else {
+  } 
+  else {
     Serial.print("Data send error. Status code: ");
     Serial.println(statusCode);
   }
